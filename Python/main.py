107f2658745471598b5929cdd6b47dfbc332e547
Sgss123 <103869422+Sgss123@users.noreply.github.com>
2022-09-13 00:01:05 +0800

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,11 @@
 #coding:utf-8
+import random
+import os
 class Body_Self(x,y,z):
     def __init__(self):
         self.x=x
         self.y=y
         self.z=z#瞎写的不至于让文件空荡荡一片
+        pass
     def getself():
         pass
